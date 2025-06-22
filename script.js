893e94a198f7643c2efd096afb4fa3747bd63838
Aniket Tikudave <anikettikudave187@gmail.com>
2025-06-22 13:07:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function updateTimer(){
+	const timerElement=document.getElementById("timer");
+	const now=new Date();
+
+	const options = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true
+  };
+
+	timerElement.textContent=now.toLocaleString("en-US",options);
+}
+
+updateTimer();
+setInterval(updateTimer,1000);
+
+
